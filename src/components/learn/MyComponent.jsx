53d256de504f857f940005f478d4d009ae5c7655
AnAnh2k <anducanh125@gmail.com>
2026-02-25 21:36:04 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,11 @@
+import "./style.css";
 const MyComponent = () => {
   return (
     <>
       <h1>Cố lên có công mài sắt có ngày nên duyên</h1>
+      <div style={{ border: "1px solid #ccc" }} className="child">
+        HEHEHEH
+      </div>
     </>
   );
 };
