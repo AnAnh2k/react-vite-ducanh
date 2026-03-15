de97f94195bc539f9ff994612a0e752bf54ab8e6
AnAnh2k <anducanh125@gmail.com>
2026-03-15 17:11:22 +0700

    Xử Lý Sự Kiện onChange với File

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,13 +1,32 @@
 import { Button, Drawer } from "antd";
-import React from "react";
+import React, { useState } from "react";
 
 const ViewUserDetail = (props) => {
   const { openDetail, setOpenDetail, dataDetail, setDataDetail } = props;
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [preview, setPreview] = useState(null);
 
   const resetAndCloseModal = () => {
     setOpenDetail(false);
     setDataDetail(null);
   };
+
+  const handleOnchangeFile = (event) => {
+    if (!event.target.files || event.target.files.length === 0) {
+      selectedFile(null);
+      setPreview(null);
+      return;
+    }
+
+    // I've kept this example simple by using the first image instead of multiple
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      setPreview(URL.createObjectURL(file));
+      console.log("file: ", file);
+    }
+  };
+  console.log("preview: ", preview);
   return (
     <Drawer
       width={"40vw"}
@@ -58,8 +77,30 @@ const ViewUserDetail = (props) => {
             >
               Upload avatar
             </label>
-            <input type="file" id="avatar" hidden />
+            <input
+              type="file"
+              id="avatar"
+              onChange={(event) => {
+                handleOnchangeFile(event);
+              }}
+              hidden
+            />
           </div>
+          {preview && (
+            <div>
+              <p style={{ marginBottom: "20px" }}>Preview:</p>
+              <img
+                style={{
+                  height: "150px",
+                  width: "150px",
+                  objectFit: "cover",
+                  border: "1px solid #ccc",
+                }}
+                src={preview}
+                alt="Preview"
+              />
+            </div>
+          )}
         </div>
       ) : (
         <div>No data</div>
