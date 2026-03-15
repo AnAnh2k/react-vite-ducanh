5fb3e63f2fb895be72b9895bbffc6a2ff7e5eda7
AnAnh2k <anducanh125@gmail.com>
2026-03-15 12:33:20 +0700

    Hiển Thị Avatar User

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,4 +1,4 @@
-import { Drawer } from "antd";
+import { Button, Drawer } from "antd";
 import React from "react";
 
 const ViewUserDetail = (props) => {
@@ -10,17 +10,56 @@ const ViewUserDetail = (props) => {
   };
   return (
     <Drawer
+      width={"40vw"}
       title="User Detail"
       closable={{ "aria-label": "Close Button" }}
       onClose={() => resetAndCloseModal()}
       open={openDetail}
     >
       {dataDetail ? (
-        <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+        <div
+          style={{
+            display: "flex",
+            gap: "15px",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
           <div>ID: {dataDetail?._id}</div>
           <div>Full Name: {dataDetail?.fullName}</div>
           <div>Email: {dataDetail?.email}</div>
           <div>Phone: {dataDetail?.phone}</div>
+          <div>
+            <p style={{ marginBottom: "20px" }}>Avatar:</p>
+            <img
+              style={{
+                height: "150px",
+                width: "150px",
+                objectFit: "cover",
+                border: "1px solid #ccc",
+              }}
+              src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail?.avatar}`}
+              alt="Avatar"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="avatar"
+              style={{
+                display: "block",
+                width: "fit-content",
+                marginTop: "15px",
+                padding: "8px 18px",
+                background: "#1677ff",
+                borderRadius: "5px",
+                cursor: "pointer",
+                color: "#fff",
+              }}
+            >
+              Upload avatar
+            </label>
+            <input type="file" id="avatar" hidden />
+          </div>
         </div>
       ) : (
         <div>No data</div>
