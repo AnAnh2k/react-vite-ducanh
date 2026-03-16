0e9d15da1fad494de05997427907566aa7782dc8
AnAnh2k <anducanh125@gmail.com>
2026-03-16 10:12:13 +0700

    Sử Dụng Uncontrolled Component Cho Register

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,73 @@
+import { Button, Checkbox, Form, Input } from "antd";
 import React from "react";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <>
+      <Form
+        form={form}
+        name="basic"
+        layout="vertical"
+        style={{
+          maxWidth: 600,
+          margin: "0 auto",
+          marginTop: "50px",
+          border: "1px solid #ccc",
+          padding: "20px",
+          borderRadius: "8px",
+        }}
+        onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <h1>Register a User</h1>
+        <br />
+        <Form.Item
+          label="Full Name"
+          name="fullName"
+          rules={[{ required: true, message: "Please input your full name!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          label="Phone Number"
+          name="phone"
+          rules={[
+            { required: true, message: "Please input your phone number!" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={null}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
 };
 
 export default RegisterPage;
